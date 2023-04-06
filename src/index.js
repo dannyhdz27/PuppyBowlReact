e1512feefafd2b87977ec9eb97e78f331cfdcd8f
Daniel H <dannyhdz27@gmail.com>
2023-04-06 11:00:17 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client";
+
+const BasicComponentNameHere = () => {
+  return (
+    <div>
+      {" "}
+      <p>Hello World!</p>{" "}
+    </div>
+  );
+};
+
+const appElement = document.getElementById("app");
+const root = createRoot(appElement);
+root.render(<BasicComponentNameHere />);
